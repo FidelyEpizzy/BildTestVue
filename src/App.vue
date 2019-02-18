a60a06f83e0f6fa3087313f9eddb7d4b27182734
MacakTco <qoredj@gmail.com>
2019-02-18 16:58:36 +0100

    non styled footer 1.02

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
   <div>
     <header-test></header-test>
+    <div class="content"></div>
+
+    <app-footer class="footer"></app-footer>
   </div>
 </template>
 
 <script>
-import '../public/fonts.css';
-import Quote from "./components/Quote.vue";
+import "../public/fonts.css";
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
-    "header-test": Header
+    headerTest: Header,
+    appFooter: Footer
   }
 };
 </script>
 
 <style scoped >
-
+html {
+  position: relative;
+  min-height: 100%;
+}
+body {
+  /* Margin bottom by footer height */
+  margin-bottom: 60px;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height:120px;
+}
 .container {
   max-width: 970px;
 }
-
-
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="conainer-fluid appFooter">
+    <div class="container">
+      <div class="row">
+          <div class="col-7">
+          <h3>ARE YOU READY TO BE BLOWN AWAY?</h3>
+          </div>
+          <div class="col-5" id="buttonFooterSection">
+              <button type="button" class="buttonFooter"> CLICK HERE TO FIND OUT</button>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style >
+.container {
+    max-width: 970px;
+}
+.appFooter {
+    background-color:black; 
+}
+h3 {
+    color: white;
+}
+#buttonFooterSection{
+    display:flex;
+    justify-content: flex-end;
+}
+.buttonFooter {
+    background-color: rgb(8, 250, 149);
+    border:none;
+   
+}
+</style>
